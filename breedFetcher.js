2bf6de96bd20b5db0e11e014a767c8e20d146b5e
Hind <hind.nayhi@gmail.com>
2022-08-04 09:55:18 -0600

    Added a callback function to breedFetcher + Implemented Mocha and Chai to check 2 test cases.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,30 @@
 const request = require('request');
 
-const breed = process.argv.slice(2);
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
-request(URL, (error, response, body) => {
+const fetchBreedDescription = function(breed, callback) {
   
-  if (error) {
-    console.log('Request failed. Check URL submitted.');
-    process.exit();
-  }
-
-  if (!error) {
-    const data = JSON.parse(body);
-    if (!data.length) {
-      console.log(`The breed name requested: "${breed}", does not exist.`);
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  
+  request(URL, (error, response, body) => {
+  
+    if (error) {
+      error = 'Request failed. Check URL submitted.';
+      callback(error, null);
     }
-    if (data.length) {
-      console.log(data[0].description);
-    }
-  }
-});
 
-//console.log('type', typeof data);
+    if (!error) {
+      const data = JSON.parse(body);
+      if (!data.length) {
+        error = `The breed name requested "${breed}", does not exist/ invalid.`;
+        callback(error, null);
+      }
+      if (data.length) {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+
+//console.log('type', typeof data); => object after JSON.parse().
